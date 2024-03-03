2ef7b853327d1ae5bbbf8c6039a0dc3d2a087505
riccardo sensi <riccardosensi57@gmail.com>
2024-03-03 18:21:31 +0100

    finita zona signUp

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,30 +1,162 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import BackgroundImage from "../components/BackgroundImage";
 import Header from "../components/Header";
+import { firebaseAuth } from "../utils/firebase-config";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { Navigate, useNavigate } from "react-router-dom";
+
 export default function Signup() {
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSignIn = async () => {
+    try {
+      const { email, password } = formValues;
+      await createUserWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) navigate("/");
+  });
+
   return (
-    <Container>
+    <Container showPassword={showPassword}>
       <BackgroundImage />
-      <Header />
-      <div className="body flex column a-center-j-center">
-        <div className="text flex column">
-          <h1>Film illimitati, Serie TV e molto altro</h1>
-          <h4>Guardami ovunque. Eliminami in ogni momento</h4>
-          <h6>
-            Pronto per iniziare? Inserisci la tua email per creare un Account o
-            accedi con uno esistente
-          </h6>
+      <div className="content">
+        <Header login />
+        <div className="body flex column a-center-j-center">
+          <div className="text flex column">
+            <h1>Film illimitati, Serie TV e molto altro</h1>
+            <h4>Guardami ovunque. Eliminami in ogni momento</h4>
+            <h6>
+              Pronto per iniziare? Inserisci la tua email per creare un Account
+              o accedi con uno esistente
+            </h6>
+          </div>
+          <div className="form ciao">
+            <input
+              type="email"
+              placeholder="Indirizzo Email"
+              name="email"
+              value={formValues.email}
+              onChange={(e) =>
+                setFormValues({
+                  ...formValues,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
+            {showPassword && (
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={formValues.password}
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+              />
+            )}
+            {!showPassword && (
+              <button onClick={() => setShowPassword(true)}>Iniziamo</button>
+            )}
+          </div>
+          <div className="ciao3">
+            <button className="ciao2" onClick={handleSignIn}>
+              Sign Up
+            </button>
+          </div>
         </div>
-        <div className="form">
-          <input type="email" placeholder="Indirizzo Email" name="email" />
-          <input type="password" placeholder="Password" name="password" />
-          <button>Iniziamo</button>
-        </div>
-        <button>Login</button>
       </div>
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+    .ciao {
+      display: flex;
+      flex-wrap: nowrap;
+      justify-content: center;
+    }
+    .ciao2 {
+      max-width: 8rem; /* Rimuovi o modifica questa riga */
+      width: 100%;
+    }
+    .ciao3 {
+      display: flex;
+      justify-content: center;
+    }
+    .body {
+      gap: 1rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        h1 {
+          padding: 0 25rem;
+        }
+      }
+      .form {
+        /* display: grid; */
+        grid-template-columns: ${({ showPassword }) =>
+          showPassword ? "1fr 1fr" : "2fr 1 fr"};
+        input {
+          color: black;
+          border: none;
+          padding: 1rem;
+          font-size: 1.2rem;
+          border: 1px solid black;
+          border-radius: 1rem;
+          &:focus {
+            outline: none;
+          }
+        }
+        button {
+          padding: 0.5rem 1rem;
+          background-color: #e50914;
+          border: none;
+          cursor: pointer;
+          color: white;
+          border-radius: 0.5rem;
+          font-weight: bolder;
+          font-size: 1.05rem;
+        }
+      }
+      button {
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        cursor: pointer;
+        color: white;
+        border-radius: 0.2rem;
+        font-weight: bolder;
+        font-size: 1.05rem;
+      }
+    }
+  }
+`;
