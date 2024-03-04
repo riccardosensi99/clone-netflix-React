aaa44938957bed105ab0c9a700eae7dd6f492126
riccardo sensi <riccardosensi57@gmail.com>
2024-03-04 18:28:01 +0100

    completata navbar e funzione di logout con ritorno nel login

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,7 +7,7 @@ import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
 } from "firebase/auth";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
   const [showPassword, setShowPassword] = useState(false);
